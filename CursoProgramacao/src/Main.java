6bda85100e75ee31da8f7eca96b70ba5c128aa87
LucaDLuffy <LucasRafaeIsaac@gmail.com>
2023-08-22 15:28:35 -0300

    testando o git no eclipse

--- CursoProgramacao/src/Main.java
+++ CursoProgramacao/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá Mundo!!");
+		System.out.println("Olá Mundo!! estamos testando o Git Aqui pra ve se vai pelo Eclipse");
 	}
 
 }
