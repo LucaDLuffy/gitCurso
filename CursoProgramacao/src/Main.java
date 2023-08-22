ddc4172e17aeb1899949a2155dfdec9c06c92808
LucaDLuffy <LucasRafaeIsaac@gmail.com>
2023-08-22 15:40:06 -0300

    testando agora usando a chave SSH

--- CursoProgramacao/src/Main.java
+++ CursoProgramacao/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá Mundo!! estamos testando o Git Aqui pra ve se vai pelo Eclipse");
+		System.out.println("Olá Mundo!! estamos testando o Git Aqui pra ve se vai pelo Eclipse"
+				+ "continuando o teste agora usando o SSH");
 	}
 
 }
